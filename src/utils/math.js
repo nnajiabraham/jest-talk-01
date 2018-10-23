2a6f1a2445cb90202695aa4e778b16713ac67829
lddv <leodias.vasconcelos@yahoo.com.br>
2018-10-23 16:44:23 -0500

    no tests yet

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,11 +1,15 @@
 export const add = (a, b) => a + b;
+
 export const minus = (a, b) => a - b;
+
 export const times = (a, b) => {
     const result = a * b;
     return result === -0 ? 0 : result;
 };
+
 export const divide = (a, b) => {
     const result = a / b;
     return result === -0 ? 0 : result;
 };
+
 export const percent = (num) => parseFloat(num) / 100;
